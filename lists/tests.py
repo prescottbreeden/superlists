9353d14b40453741c229a9176171a17c04619c50
prescottbreeden <prescott.breeden@gmail.com>
2018-10-30 00:15:34 -0700

    first function/unit-test cycle completed

--- lists/tests.py
+++ lists/tests.py
@@ -2,5 +2,11 @@ from django.test import TestCase
 
 
 class HomePageTest(TestCase):
-    def test_smoke_test(self):
-        self.assertEqual(1+1, 3)
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertIn(
+            '<title>To-Do Lists</title>',
+            response.content.decode()
+        )
+        self.assertTrue(response.content.decode().startswith('<html>'))
+        self.assertTrue(response.content.decode().endswith('</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home_page(request):
+    return HttpResponse("<html><title>To-Do Lists</title><h1>To-Do</h1></html>")
